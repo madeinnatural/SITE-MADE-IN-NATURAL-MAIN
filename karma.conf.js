867c69db91e643da5ee73edcdc1ce97cdc69070f
natan danilo <natan.danilo@gmail.com>
2022-06-18 13:51:04 -0300

    Mudança de versão angular

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/site-made-in-natural'),
+      dir: require('path').join(__dirname, './coverage/SITE-MADE-IN-NATURAL'),
       subdir: '.',
       reporters: [
         { type: 'html' },
