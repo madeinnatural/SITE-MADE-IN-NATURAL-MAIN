e7f35365e174b14001f3bcd1020b9d71049e4aec
natan danilo <natan.danilo@gmail.com>
2022-06-18 14:20:45 -0300

    Home componentizado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { ProductsCartComponent } from './products-cart/products-cart.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    CarrouselComponent,
+    ProductsCartComponent
   ],
   imports: [
     CommonModule,
     ComponentsRoutingModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    CarrouselComponent,
+    ProductsCartComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,37 +15,8 @@ export class HomeComponent implements OnInit {
   }> = [];
 
   constructor() {
-    this.listProduct = [
-      {
-        name: 'Produto 1asdasd',
-        price: 2.5,
-        weigth: 1.0,
-        category: 'Categoria 1',
-        amount: 0,
-      },
-    ];
   }
 
-  showProductsAll() {
-    const productsListServer = [
-      {
-        name: 'Sabonete',
-        price: 3.5,
-        weigth: 1.0,
-        category: 'Categoria 1',
-        amount: 0,
-      },
-      {
-        name: 'Perfeuma',
-        price: 4.5,
-        weigth: 1.0,
-        category: 'Categoria 1',
-        amount: 0,
-      },
-    ];
-
-    this.listProduct.push(...productsListServer);
-  }
 
   ngOnInit(): void {}
 }
